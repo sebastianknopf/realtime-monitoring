2e6f730a6ca2f6fce849214492918325d8a12842
mail@sebastian-knopf.de <mail@sebastian-knopf.de>
2026-03-19 21:49:47 +0100

    removed deprecated Win start script

--- startup.ps1
+++
@@ -1,4 +0,0 @@
-docker run --rm `
-  -v "${PWD}\config\dev.yaml:/app/config/config.yaml" `
-  -v "${PWD}\sampledata\realtimeobserver.db3:/app/data/data.db3" `
-  sebastianknopf/realtimeobserver:latest
